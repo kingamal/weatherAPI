77d5f49a81423d6ff0230cdceb31de021f2ddc51
Kinga <kinga.maleszewska@o2.pl>
2021-07-05 22:29:25 +0200

    data jutrzejsza

--- weather.py
+++ weather.py
@@ -2,6 +2,7 @@ import requests
 import sys
 import json
 from datetime import datetime
+import datetime
 from os.path import getmtime
 
 class Weather:
@@ -22,14 +23,14 @@ class Weather:
         sec = getmtime(file)
         now = datetime.now().timestamp()
         if now - sec < 60 * 60 * 24:
-            with open(file, "r") as f:
+            with open(file, "r+") as f:
                 self.response = json.load(f)
         else:
             self.get_response(key)
             self.save_response(file)
 
     def save_response(self, file):
-        with open(file, 'w') as fp:
+        with open(file, 'a') as fp:
             file_content_json = json.dumps(self.response)
             fp.write(file_content_json)
         return True
@@ -37,7 +38,7 @@ class Weather:
     def forecast(self):
         what_weather = {}
         for day in self.response["list"]:
-            date = datetime.utcfromtimestamp(day["dt"]).strftime("%Y-%m-%d")
+            date = datetime.utcfromtimestamp(day["dt"]).strftime("%Y-%m-%d") #unixtime
             forecast = day["weather"][0]["main"]
             what_weather[date] = forecast
         return what_weather
@@ -48,11 +49,18 @@ key = input()
 outfile = sys.argv[1]
 weather.load_response(key, outfile)
 dictionary = weather.forecast()
-if sys.argv[2] in dictionary:
-    if dictionary[sys.argv[2]] == 'Rain':
+if not sys.argv[2]:
+    tomorrow = datetime.date.today() + datetime.timedelta(days=1)
+    if dictionary[sys.argv[2]] == 'Rain' or dictionary[sys.argv[2]] == 'Snow':
         print('Bedzie padac')
     else:
         print('Nie bedzie padac')
 else:
     print('Nie wiem')
-#unixtime
+if sys.argv[2] in dictionary:
+    if dictionary[sys.argv[2]] == 'Rain' or dictionary[sys.argv[2]] == 'Snow':
+        print('Bedzie padac')
+    else:
+        print('Nie bedzie padac')
+else:
+    print('Nie wiem')
