ae132b2aaa954fe95371fb87b1042a4ea7d16c40
Kinga <kinga.maleszewska@o2.pl>
2021-07-06 18:41:58 +0200

    all works

--- weather.py
+++ weather.py
@@ -1,9 +1,9 @@
 import requests
 import sys
 import json
-from datetime import datetime
-import datetime
-from os.path import getmtime
+from datetime import datetime, date, timedelta
+from os.path import getmtime, exists
+
 
 class Weather:
     def __init__(self):
@@ -17,12 +17,15 @@ class Weather:
             'x-rapidapi-host': "community-open-weather-map.p.rapidapi.com"
             }
         self.response = requests.request("GET", url, headers=headers, params=querystring).json()
-        print(self.response)
+        # print(self.response)
 
     def load_response(self, key, file):
+        if not exists(file):
+            self.get_response(key)
+            self.save_response(file)
         sec = getmtime(file)
         now = datetime.now().timestamp()
-        if now - sec < 60 * 60 * 24:
+        if now - sec < 60 * 60:
             with open(file, "r+") as f:
                 self.response = json.load(f)
         else:
@@ -30,7 +33,7 @@ class Weather:
             self.save_response(file)
 
     def save_response(self, file):
-        with open(file, 'a') as fp:
+        with open(file, 'w') as fp:
             file_content_json = json.dumps(self.response)
             fp.write(file_content_json)
         return True
@@ -49,15 +52,16 @@ key = input()
 outfile = sys.argv[1]
 weather.load_response(key, outfile)
 dictionary = weather.forecast()
-if not sys.argv[2]:
-    tomorrow = datetime.date.today() + datetime.timedelta(days=1)
-    if dictionary[sys.argv[2]] == 'Rain' or dictionary[sys.argv[2]] == 'Snow':
+if len(sys.argv) < 3:
+    tomorrow = date.today() + timedelta(days=1)
+    tomorrow = tomorrow.strftime("%Y-%m-%d")
+    if tomorrow not in dictionary:
+        print('Nie wiem')
+    elif dictionary[tomorrow] == 'Rain' or dictionary[tomorrow] == 'Snow':
         print('Bedzie padac')
     else:
         print('Nie bedzie padac')
-else:
-    print('Nie wiem')
-if sys.argv[2] in dictionary:
+elif sys.argv[2] in dictionary:
     if dictionary[sys.argv[2]] == 'Rain' or dictionary[sys.argv[2]] == 'Snow':
         print('Bedzie padac')
     else:
